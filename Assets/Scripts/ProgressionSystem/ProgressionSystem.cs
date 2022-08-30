5d7788dd61cffd7eb96075ca499af4ce35fbedde
WilUll <90192382+WilUll@users.noreply.github.com>
2022-08-30 13:09:56 +0200

    New Input

--- Assets/Scripts/ProgressionSystem/ProgressionSystem.cs
+++ Assets/Scripts/ProgressionSystem/ProgressionSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchInteract : MonoBehaviour
+public class ProgressionSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
